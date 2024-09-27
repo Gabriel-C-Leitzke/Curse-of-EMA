c7debcf4d74f1888ebb0b45ca4264b685dae207b
Gabriel-C-Leitzke <gabrielleitzke3@outlook.com>
2024-09-26 21:08:58 -0400

    ajustando a página inicial

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -6,11 +6,6 @@ function login(event) {
   
     // Verifica se o usuário e a senha estão corretos (no exemplo, são "admin" e "admin")
     if (email && password === "admin") {
-      // Simulando a geração de um token JWT após o login bem-sucedido
-
-      // const token = "seu_token_jwt_aqui";
-      // localStorage.setItem("token", token); // Armazena o token no localStorage
-      
       window.location = "../index.html"; // Redireciona para a página inicial
     } else {
       alert("Saia daqui seu colocador de Usuário e senha errado!");
